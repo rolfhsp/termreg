52ebf6d38721d8f07fedb6891312dc7065cec6be
Rolf Pedersen <rolfhsp@gmail.com>
2013-02-12 23:18:59 +0100

    Initial commit.

---
+++ README.md
@@ -0,0 +1,6 @@
+# Terminal Register
+
+Norwegian terminal register.
+
+Created by [Rolf Pedersen](mailto:rolfhsp@gmail.com)
+ 
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Termreg::Application.config.secret_token = 'fa300ce77d673816946120de54530257979c5472c616ff3a48943117b613a99acaabb9f8c8764144b688581580fe170c8a20c2b74490c450c4d1cffe26d9d564'
